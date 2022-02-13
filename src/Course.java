2e87842ac2554d4b851188f895c175da6136edd8
Yumatov Maksim <yumatof@ya.ru>
2022-02-13 20:32:29 +0300

    Homework#1.add get method in course class + create object course class

--- src/Course.java
+++ src/Course.java
@@ -1,14 +1,30 @@
 public class Course {
 
     class ObstacleBuilder {
+        private String titleObstacle;
         private String nameObstacle;
         private float minNormativeResult;
         private float maxNormativeResult;
 
-        public ObstacleBuilder(String nameObstacle,float minNormativeResult,float maxNormativeResult){
+        public ObstacleBuilder(String titleObstacle, String nameObstacle,float minNormativeResult,float maxNormativeResult){
+            this.titleObstacle=titleObstacle;
             this.nameObstacle=nameObstacle;
             this.minNormativeResult=minNormativeResult;
             this.maxNormativeResult=maxNormativeResult;
+        }
+        public String getTitleObstacle(){
+            return titleObstacle;
+        }
+        public String getNameObstacle(){
+            return nameObstacle;
+        }
+
+        public float getMinNormativeResult() {
+            return minNormativeResult;
+        }
+
+        public float getMaxNormativeResult() {
+            return maxNormativeResult;
         }
     }
 
@@ -19,16 +35,17 @@ public class Course {
     }
 
     private ObstacleBuilder[] createObstacleCourse(){
-        ObstacleBuilder[] obstacleCourse = {new ObstacleBuilder("Бег на дистанцию 100 метров", 10.64f,14.4f),
-        new ObstacleBuilder("Прыжки в длинну",4, 7.1f),
-        new ObstacleBuilder("Прыжки в высоту", 1.3f,2)};
+        ObstacleBuilder[] obstacleCourse = {new ObstacleBuilder("run","Бег на дистанцию 100 метров", 10.64f,14.4f),
+        new ObstacleBuilder("jumpLong", "Прыжки в длинну",4, 7.1f),
+        new ObstacleBuilder("jumpHeight","Прыжки в высоту", 1.3f, 2.1f)};
         return obstacleCourse;
     }
 
     public void passingCourse(TeamBuilder team){
         for (int i=0; i<obstacleCourse.length; i++){
-            for (int j=0;j<team.lineUp.length; j++){
-                System.out.println(team.lineUp[j].getMemberName());
+            for(TeamBuilder.TeamMember t:team.lineUp){
+            //for (int j=0;j<team.lineUp.length; j++){
+                System.out.println(t.getMemberName());
 
 
             }
