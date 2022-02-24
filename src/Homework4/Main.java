52e95e58371021fe32bb4c2b0201f85883281ee7
Yumatov Maksim <yumatof@ya.ru>
2022-02-24 21:58:24 +0300

    Homework#4 add solution case#1

--- src/Homework4/Main.java
+++ src/Homework4/Main.java
@@ -1,9 +1,38 @@
 package Homework4;
 
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Main {
 
     public static void main(String[] args){
+//case#1
+        String[] duplicateStr = {"слово","пикник","сила","обочина","реверс",
+                                 "повтор","стопор","ротор","слово","сила",
+                                 "пикник","стопор","обочина","ротор","пикник"};
+
+        System.out.println(findDuplicateInArray(duplicateStr).toString());
 
 
+//case#2
+
+
+    }
+
+    public static <T> HashMap<T, Integer> findDuplicateInArray (T[]tmpArray){
+
+        HashSet hs = new HashSet();
+        HashMap<T, Integer> hm = new HashMap<>();
+
+        for(T element:tmpArray){
+            if (hs.add(element)){
+               hm.put(element, 1);
+            } else{
+                hm.put(element, (hm.get(element)+1));
+            }
+
+        }
+        return hm;
     }
 }
